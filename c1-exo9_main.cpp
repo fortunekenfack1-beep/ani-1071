193722b0eb6b3d79bdf2a7897dc2fc3be3964607
fortunekenfack1-beep <fortunekenfack1@gmail.com>
2026-09-18 23:05:43 +0100

    Add files via upload

---
+++ c1-exo9_main.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main () {
+    int n ; 
+    printf ("entrer un  nombre entier  \n ") ;
+    scanf("%d" , & n) ;
+    printf("le double de ce nombre est : %d " ,   n*2 );
+    return 0 ;
+    
+
+}
