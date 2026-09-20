583da0beae2957a688aa278220c82ae55925fdd3
fortunekenfack1-beep <fortunekenfack1@gmail.com>
2026-09-20 01:01:40 +0100

    Add files via upload

---
+++ chapitre-01/c1-demo2_main.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+int main () {
+    printf("####################\n") ;
+    printf("#                  #\n") ;
+    printf("#                  #\n") ;
+    printf("#                  #\n") ;
+    printf("#                  #\n") ;
+    printf("#                  #\n") ;
+    printf("####################\n") ;
+    return 0 ;
+}
