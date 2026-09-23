f376a6e1e2d234cbf878d09e2de6e75537ef1e85
fortunekenfack1-beep <fortunekenfack1@gmail.com>
2026-09-23 11:06:12 +0100

    Add rectangle drawing using loops in C++

--- chapitre-02/exo1-le_rectangle_en_boucles/c2-exo1_main.cpp
+++ chapitre-02/exo1-le_rectangle_en_boucles/c2-exo1_main.cpp
@@ -1,1 +1,16 @@
+#include <cstdio>
+int main () {
+    int i , j;
 
+    for (i=1 ; i<=7 ; i++) {
+    for (j=1 ; j<=20 ; j++) {
+        if (i==1 || i==7 || j==1 || j==20) {
+            printf("#") ;
+        } else {
+            printf(" ") ;
+        }
+    }
+    printf("\n");
+    }
+    return 0;
+}
