ad100b34d1416de35ff630ac91238ccb4535ffc3
fortunekenfack1-beep <fortunekenfack1@gmail.com>
2026-09-25 15:51:03 +0100

    Print current number in Collatz sequence
    Added output of current number during Collatz sequence.

--- chapitre-02/exo10-la_suite_de_collatz/c2-exo10_main.cpp
+++ chapitre-02/exo10-la_suite_de_collatz/c2-exo10_main.cpp
@@ -4,6 +4,7 @@ int main () {
     printf("entrer un nombre");
     scanf("%d" , & n);
     while (n!=1) { 
+        printf("%d\n" , n) ;
         if (n%2==0){
             n=n/2 ; 
         }
@@ -17,15 +18,3 @@ int main () {
     printf(" le nombre d'etapes est %d" , cmp);
     return 0 ; 
     }
-
-
-en verifiant le code avec 27 on a 
-```
-chris@DESKTOP-RSAB6V8 MSYS /c/Users/chris/ani-1071/chapitre-02
-$ clang++ c2-exo10_main.cpp -o programme
-
-chris@DESKTOP-RSAB6V8 MSYS /c/Users/chris/ani-1071/chapitre-02
-$ ./programme
-entrer un nombre27
- le nombre d'etapes est 111
-    ```
