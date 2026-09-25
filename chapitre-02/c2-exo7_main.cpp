25b44fe3a6d1d89e76f47619a6825142052d8fe4
fortunekenfack1-beep <fortunekenfack1@gmail.com>
2026-09-25 17:13:01 +0100

    Add files via upload

---
+++ chapitre-02/c2-exo7_main.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+int main () { 
+    int menu ; 
+    printf("1.Nouvelle partie\n") ;
+    printf("2.Charger une partie\n") ;
+    printf("3.options\n") ;
+    printf("4.Quitter\n") ;
+    scanf("%d" , &menu) ;
+    switch(menu) {
+        case 1 : 
+            printf("nouvelle partie\n") ;
+            break ;
+        case 2 : 
+            printf("charger une partie\n") ;
+            break ;
+        case 3 : 
+            printf("options\n") ;
+            break ;
+        case 4 : 
+            printf("quitter\n") ;
+            break ;
+        default :
+            printf("erreur de saisie\n") ;
+    }
+    return 0 ;
+}
