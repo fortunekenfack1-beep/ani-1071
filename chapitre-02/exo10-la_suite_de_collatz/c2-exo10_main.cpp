dabf6956d8d7b9f8a96647bd28efe5ec065a0d61
fortunekenfack1-beep <fortunekenfack1@gmail.com>
2026-09-25 09:54:01 +0100

    Implement Collatz sequence step counter
    The code calculates the number of steps to reach 1 using the Collatz conjecture for a given number.

--- chapitre-02/exo10-la_suite_de_collatz/c2-exo10_main.cpp
+++ chapitre-02/exo10-la_suite_de_collatz/c2-exo10_main.cpp
@@ -17,3 +17,15 @@ int main () {
     printf(" le nombre d'etapes est %d" , cmp);
     return 0 ; 
     }
+
+
+en verifiant le code avec 27 on a 
+```
+chris@DESKTOP-RSAB6V8 MSYS /c/Users/chris/ani-1071/chapitre-02
+$ clang++ c2-exo10_main.cpp -o programme
+
+chris@DESKTOP-RSAB6V8 MSYS /c/Users/chris/ani-1071/chapitre-02
+$ ./programme
+entrer un nombre27
+ le nombre d'etapes est 111
+    ```
