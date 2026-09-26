0c606d3cc203442af7d95e45562fd695fdca5c2f
fortunekenfack1-beep <fortunekenfack1@gmail.com>
2026-09-26 20:51:33 +0100

    Add files via upload

---
+++ chapitre-02/c2-demo3_main.cpp
@@ -0,0 +1,14 @@
+ #include <cstdio>
+ int main(){
+        printf("bool            :%zu octets\n", sizeof(bool));
+        printf("char            :%zu octets\n", sizeof(char));
+        printf("short           :%zu octets\n", sizeof(short));  
+        printf("int             :%zu octets\n", sizeof(int));
+        printf("long            :%zu octets\n", sizeof(long));
+        printf("long long       :%zu octets\n", sizeof(long long)); 
+        printf("unsigned char   :%zu octets\n", sizeof(unsigned char));
+        printf("unsigned int    :%zu octets\n", sizeof(unsigned int ));
+        printf("double          :%zu octets\n", sizeof(double));
+        printf("long   double   :%zu octets\n", sizeof(long double)); 
+        return 0 ;
+ }
